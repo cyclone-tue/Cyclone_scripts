e5e121734b817356320861ee6e0cddd2fbd55438
Michiel <m.p.favier@student.tue.nl>
2018-01-17 12:11:05 +0100

    Added output on localhost:14550

--- mavproxy-base.sh
+++ mavproxy-base.sh
@@ -3,4 +3,4 @@
 stty -F /dev/ttyACM0 57600 raw -echo -echoe -echok -iexten -echoctl -echoke
 socat UDP-DATAGRAM:127.0.0.1:13320 file:/dev/ttyACM0
 
-mavproxy.py --master=/dev/ttyUSB0 --load-module=DGPS --cmd="set shownoise false"
+mavproxy.py --master=/dev/ttyUSB0 --out=udp:127.0.0.1:14550 --load-module=DGPS --cmd="set shownoise false"
