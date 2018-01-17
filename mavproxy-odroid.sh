385e04d7164de1764b4a4d2943f6fe60debef7ae
Cyclone <cyclone@cyclone-eindhoven.nl>
2018-01-17 05:43:38 -0500

    Remove noise from mavproxy

--- mavproxy-odroid.sh
+++ mavproxy-odroid.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-mavproxy.py --master=/dev/ttyS1 --out=/dev/ttyUSB0 --load-module=droneapi.module.api
+mavproxy.py --master=/dev/ttyS1 --out=/dev/ttyUSB0 --load-module=droneapi.module.api --cmd="set shownoise false"
 
 exit
