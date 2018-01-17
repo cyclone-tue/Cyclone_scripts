d7a304898fa1c9a44e611bf976e309a99d7e4529
Michiel <m.p.favier@student.tue.nl>
2018-01-17 11:46:34 +0100

    Updated base script

--- mavproxy-base.sh
+++ mavproxy-base.sh
@@ -3,4 +3,4 @@
 stty -F /dev/ttyACM0 57600 raw -echo -echoe -echok -iexten -echoctl -echoke
 socat UDP-DATAGRAM:127.0.0.1:13320 file:/dev/ttyACM0
 
-mavproxy.py --master=/dev/ttyUSB0 --load-module=DGPS 
+mavproxy.py --master=/dev/ttyUSB0 --load-module=DGPS --cmd="set shownoise false"
