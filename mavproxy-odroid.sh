ebfa233f1d444f7fa54b1068857a11ece22a16fd
Cyclone <cyclone@cyclone-eindhoven.nl>
2018-01-17 07:34:12 -0500

    Updated BAUDrate

--- mavproxy-odroid.sh
+++ mavproxy-odroid.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-mavproxy.py --master=/dev/ttyS1 --out=/dev/ttyUSB0 --cmd="set shownoise false"
+mavproxy.py --master=/dev/ttyS1 --out=/dev/ttyUSB0 --baudrate=115200 --cmd="set shownoise false"
 
 exit
