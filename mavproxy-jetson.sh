71036c5f550784810f30630ac380962623072bf3
Cyclone <cyclone@cyclone.nl>
2018-03-19 11:08:48 +0000

    Updated jetson script

--- mavproxy-jetson.sh
+++ mavproxy-jetson.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-screen -S MavProxy -d -m -s /bin/bash mavproxy.py --master=/dev/ttyTHS1,115200 --out=udpin:0.0.0.0:14550 --cmd="set shownoise false"
+screen -S MavProxy -d -m -s /bin/bash mavproxy.py --master=/dev/ttyTHS1,115200 --out=udpin:0.0.0.0:14550 --out=udp:127.0.0.1:14000 --cmd="set shownoise false"
 
 exit
