4dfec3ca86c245b151520dffed97b0ec1be06485
Cyclone <Cyclone@cyclone.org>
2018-04-06 04:45:27 -0400

    Updated base script for odroid

--- mavproxy-base-udp.sh
+++ mavproxy-base-udp.sh
@@ -3,4 +3,4 @@
 stty -F /dev/ttyACM0 57600 raw -echo -echoe -echok -iexten -echoctl -echoke
 socat UDP-DATAGRAM:127.0.0.1:13320 file:/dev/ttyACM0
 
-mavproxy.py --master=udpout:192.168.1.119:14550 --out=udp:127.0.0.1:14000 --load-module=DGPS --cmd="set shownoise false"
+mavproxy.py --master=udpout:192.168.1.119:14550 --out=udpin:0.0.0.0:14550 --out=udp:127.0.0.1:14000 --load-module=DGPS --cmd="set shownoise false"
